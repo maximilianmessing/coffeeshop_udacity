124da61ee04b92a3bb0dd5fa7e17c3cb94e3b1cd
Maximilian Messing <maximilian.messing@whu.edu>
2020-03-26 16:56:24 +0100

    adapt configuration for application

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
+    url: 'wcp.eu', // the auth0 domain prefix
+    audience: 'coffee_shop', // the audience set for the auth0 app
+    clientId: 'HQpDr1d96ReUvsNG7I8YKKB4fsdU0B7G', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
